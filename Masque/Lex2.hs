ec468ac1cff79848c665ad2d781d0f7e2c92ccac
Dan Connolly <dckc@madmode.com>
2015-11-29 01:45:22 -0600

    Lex2 can tokenize mafia.mt
      - flatten bracket handling in scan
      - stack close brackets, not just Bool
      - handle VERB_ASSIGN, i.e. with=

--- Masque/Lex2.hs
+++ Masque/Lex2.hs
@@ -2,11 +2,10 @@ module Masque.Lex2 where
 
 import Text.Parsec ((<|>))
 import qualified Text.Parsec as P
-import qualified Text.Parsec.Combinator as PC
 import Text.Parsec.IndentParsec(runGIPT)
 
 import Control.Monad.Identity
-import Control.Applicative ((<$>), (<*>), (*>))
+import Control.Applicative ((<$>), (<*>), (<*), (*>))
 import qualified Text.Parsec.Token as T
 import qualified Text.Parsec.IndentParsec.Token as IT
 import qualified Text.Parsec.IndentParsec.Prim as IP
@@ -29,7 +28,7 @@ langDef = T.LanguageDef {
   , T.caseSensitive = False
   }
 
-tokP :: IT.IndentTokenParser String [Bool] Identity
+tokP :: IT.IndentTokenParser String [Char] Identity
 tokP = T.makeTokenParser langDef
 
 {- |
@@ -44,51 +43,90 @@ lexSource inp =
                       Left e -> do putStr "parse error: "
                                    print e
 
-type MonteLex a = IP.IndentParsecT String [Bool] Identity a
+type MonteLex a = IP.IndentParsecT String [Char] Identity a
 
 monteTokens :: MonteLex [TagTok]
 monteTokens = do
   IT.whiteSpace tokP
-  ts <- scanMode
+  ts <- rest
   P.eof
   return ts
   where
-    scanMode = do
+    rest = do
       stack <- P.getState
       case stack of
-        (True:_) -> (:) <$> quasiPart <*> scanMode
-        _ -> run
+        ('`':_) -> (++) <$> quasiParts <*> rest
+        (_:_) -> (++) <$> scan <*> rest
+        [] -> (const [] <$> P.eof) <|> scan
 
-    run :: MonteLex [TagTok]
-    run =     (++) <$> (wrap "[" "]" <$> IT.brackets tokP run) <*> run
-          <|> (++) <$> (wrap "(" ")" <$> IT.parens tokP run) <*> run
-          <|> (++) <$> (wrap "{" "}" <$> IT.braces tokP run) <*> run
-          <|> (++) <$> quasiStart <*> scanMode
-          <|> (:) <$> monteToken <*> run
-          <|> return []
-    wrap o c ts = [(o, "")] ++ ts ++ [(c, "")]
+    scan :: MonteLex [TagTok]
+    scan =
+      (:) <$> quasiStart <*> rest
+      <|> (:) <$> (bra "[" ']') <*> rest <|> (:) <$> (ket ']') <*> rest
+      <|> (:) <$> (bra "(" ')') <*> rest <|> (:) <$> (ket ')') <*> rest
+      <|> (:) <$> (bra "{" '}') <*> rest <|> (:) <$> (ket '}') <*> rest
+      <|> (:) <$> monteToken <*> rest
+      <|> const [] <$> P.lookAhead (P.oneOf "}])")
+    bra :: String -> Char -> MonteLex TagTok
+    bra open close = do
+      v <- IT.symbol tokP open
+      P.modifyState (push close)
+      return (v, "")
+    ket :: Char -> MonteLex TagTok
+    ket close = do
+      v <- IT.symbol tokP [close]
+      stack <- P.getState
+      case stack of
+        (ch:stack')
+          | ch == close
+            -> do
+              P.setState stack'
+              return (v, "")
+        -- TODO: track position of open
+        (expected:_) -> oops (v ++ " rather than " ++ [expected])
+        [] -> oops ("extra close: " ++ v)
+    oops msg = do
+      _ <- P.unexpected msg
+      IP.tokeniser $ return ("ERROR", msg)
 
+    quasiStart :: MonteLex TagTok
     quasiStart = do
       _ <- P.char '`'
-      P.modifyState (push True)
-      return [("QUASI_OPEN", "")]
+      P.modifyState (push '`')
+      -- TODO: include quasiText in quasi_open
+      return ("QUASI_OPEN", "")
     push b s = b:s
     pop (_:s) = s
     pop [] = []  -- error? warning?
 
-    quasiPart :: MonteLex TagTok
-    quasiPart =
-          quasiEnd
-      <|> P.try (hole '$' "DOLLAR_IDENT")
-      <|> P.try (hole '@' "AT_IDENT")
-      <|> do { P.try (P.char '$');  (P.unexpected "$ escape in quasi") }
-      <|> (\txt -> ("QUASI_TEXT", txt)) <$> quasiText
+    quasiParts :: MonteLex [TagTok]
+    quasiParts =
+      (:) <$> (P.try quasiEnd) <*> return []
+      <|> (:) <$> (P.try (hole '$' "DOLLAR_IDENT")
+                   <|> P.try (hole '@' "AT_IDENT")) <*> quasiParts
+      <|> (\t ts -> collapse (t:ts)) <$> ((\txt -> ("QUASI_TEXT", txt)) <$> quasiText) <*> quasiParts
+      <|> P.unexpected "quasiText construct"
+    collapse :: [TagTok] -> [TagTok]
+    collapse (("QUASI_TEXT", s1):("QUASI_TEXT", s2):toks)
+      = ("QUASI_TEXT", s1 ++ s2):collapse toks
+    collapse toks = toks
+    quasiText :: MonteLex String
+    quasiText =
+      (double '`') <|> (double '$') <|> (double '@')
+      <|> (P.many1 (P.noneOf "`$@"))
+    double :: Char -> MonteLex String
+    double c = const [c] <$> P.try ((P.char c) *> (P.char c))
 
     quasiEnd = do
-      _ <- P.char '`'
+      _ <- lexeme '`'
+      -- TODO: balace ()s
       P.modifyState pop
       return ("QUASI_CLOSE", "")
+      where
+        lexeme c = IT.lexeme tokP $ IP.tokeniser $ sym c
+        sym c = (P.char c) <* (P.notFollowedBy (P.char c))
 
+    hole :: Char -> String -> MonteLex TagTok
     hole ch tag = (P.char ch) *> (P.try (pfxIdent tag) <|> pfxOpen ch)
     pfxIdent tag = do
       c0 <- P.satisfy ML.idStart
@@ -96,23 +134,24 @@ monteTokens = do
       return (tag, c0:c1n)
     pfxOpen ch = do
       _ <- P.char '{'
-      P.modifyState (push False)
+      P.modifyState (push '}')
       return (ch:'{':[] , "")
 
-    quasiText :: MonteLex String
-    quasiText =
-      P.many (double '@' <|> double '$' <|> double '`'
-              <|> P.noneOf "@$`")
-      where
-        double :: Char -> MonteLex Char
-        double c = const c <$> P.try (P.string (c:c:[]))
 
-
-monteToken :: IP.IndentParsecT String [Bool] Identity TagTok
-monteToken = kw <|> op <|> punct <|> ident <|> literal
+monteToken :: MonteLex TagTok
+monteToken = op <|> literal <|> punct <|> augAssign <|> kw <|> ident
   where
-    kw = go IT.reserved [kw' | (kw', _) <- ML.keywords]
-    op = go IT.reservedOp [op' | (op', _) <- ML.operators]
+    augAssign = notKw $ P.try $ IT.lexeme tokP opeq
+    opeq :: MonteLex String
+    opeq = (:) <$> (P.satisfy ML.idStart) <*> (P.many $ P.satisfy ML.idPart) <* (P.char '=')
+    notKw :: MonteLex String -> MonteLex TagTok
+    notKw p = do
+      s <- P.try p
+      if elem s kwds then do
+        P.unexpected $ "keyword in augmented assignment: " ++ s
+        else return ("VERB_ASSIGN", s)
+    kw = go IT.reserved kwds P.<?> "keyword"
+    op = go IT.reservedOp [op' | (op', _) <- ML.operators] P.<?> "operator"
     punct = go IT.reservedOp [p' | (p', _) <- ML.punctuation]
     go _ [] = error "at least 1"
     go f [t1] = go' f t1
@@ -122,10 +161,10 @@ monteToken = kw <|> op <|> punct <|> ident <|> literal
       return (t, "")
     ident = tagged "IDENTIFIER" <$> IT.identifier tokP
     tagged t s = (t, s)
-    literal = integer <|> str
+    literal = (P.try integer) <|> (P.try str)
     str = tagged ".String." <$> IT.stringLiteral tokP
     integer = shown ".int." <$> IT.integer tokP
     shown t x = (t, show x)
-
+    kwds = [kw' | (kw', _) <- ML.keywords]
 -- semiSep = IT.semiSepOrFoldedLines tokP
 
